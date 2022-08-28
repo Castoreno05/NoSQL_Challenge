4cf7826b03317ef34f804b79fbe3b0221f69225b
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-08-28 07:55:11 -0500

    Creating initial files

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const Username = require('./Username');
+const Email = require('./Email');
+const Thoughts = require('./Thoughts');
+const Friends = require('./Friends');
+
+
+module.exports = { Username, Email, Thoughts, Friends };
