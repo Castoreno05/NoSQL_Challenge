f2837775eb1b249d68409bc45246ee56332b6a80
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-08-30 09:34:02 -0500

    README.md update and data update

--- utils/data.js
+++ utils/data.js
@@ -1,11 +1,15 @@
 // Create thoughts data to seed. Use Thoughts model for referencing
 const thoughts = [
-    { thoughtText: 'Hope this challenge turns out well', username:'casto05'}
+    { thoughtText: 'Excited to see where i go from here', username:'Mat'},
+    { thoughtText: 'Getting the guys together to get bronze tonight', username: 'Bl8k'},
+    { thoughtText: 'I have so much code that i need to finish by tonight', username: 'Dab'}
 ]
 
 // Create user data to seed. Use User model for referencing
 const user = [
-    { username: "casto05", email: "cast@test.com" }
+    { username: "Bl8k", email: "Bl8k@test.com" },
+    { username: "Mat", email: "Mat@test.com" },
+    { username: "Dab", email: "dab@test.com" }
 ]
 
 module.exports = { thoughts, user };
